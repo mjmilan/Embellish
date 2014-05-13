e6ea0ebdbf48d28e513224f4f795c7ae572633a3
Martin Milan <martin@mjmilan.com>
2014-05-13 01:13:18 +0100

    Initial commit

---
+++ Embellish.Tests/IntergerExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Martin
+ * Date: 13/05/2014
+ * Time: 00:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Embellish;
+
+namespace Embellish.Tests
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	[TestFixture]
+	public class IntegerExtensionsTests
+	{
+		[Test]
+		public void TimesMethodRepeatsActionNTimes()
+		{
+			int i = 0;
+			5.Times(() => i++);
+			Assert.That(i, Is.EqualTo(5));
+		}
+	}
+}
---
+++ Embellish/IntegerExtensions.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Martin
+ * Date: 13/05/2014
+ * Time: 00:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Embellish
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public static class IntegerExtensions
+	{
+		#region Extension Methods
+		public static void Times(this int occurences, Action action)
+		{
+			for(int i = 1; i <= occurences; i++)
+			{
+				action.Invoke();
+			}
+		}
+		#endregion
+		
+	}
+}
