ee47adee390d365cd04ac019ebd196e2dd87a1f5
martinmi <martin@mjmilan.com>
2014-10-15 12:24:28 +0100

    Adding some extension method for Linq.

---
+++ Embellish.Tests/LinqEmbellishmentsTests.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Linq;
+using NUnit.Framework;
+using Embellish.LinqExtensions;
+namespace Embellish.Tests
+{
+	/// <summary>
+	/// Description of LinqEmbellishmentsTests.
+	/// </summary>
+    [TestFixture]
+	public class LinqEmbellishmentsTests
+	{
+        private int[] data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        [Test]
+        public void CheckFirstNMatchingCondition()
+        {
+            var expected = new int[] { 2, 4, 6 };
+            var firstThreeEvens = data.FirstNMatchingCondition(3, x => x % 2 == 0);
+
+            bool match = expected.SequenceEqual(firstThreeEvens);
+
+            Assert.That(match, Is.True);
+        }
+
+        [Test]
+        public void CheckLastNMatchingCondition()
+        {
+            var expected = new int[] { 16, 18, 20 };
+            var lastThreeEvens = data.LastNMatchingCondition(3, x => x % 2 == 0);
+            bool match = expected.SequenceEqual(lastThreeEvens);
+
+            Assert.That(match, Is.True);
+        }
+
+        [Test]
+        public void CheckFirstN()
+        {
+            var expected = new int[] { 1, 2, 3, 4, 5 };
+            var firstFive = data.FirstN(5);
+            bool match = expected.SequenceEqual(firstFive);
+
+            Assert.That(match, Is.True);
+        }
+
+        [Test]
+        public void CheckLastN()
+        {
+            var expected = new int[] { 16,17,18,19,20 };
+            var lastFive = data.LastN(5);
+            bool match = expected.SequenceEqual(lastFive);
+
+            Assert.That(match, Is.True);
+        }
+
+
+	}
+}
---
+++ Embellish/LinqExtensions/LinqEmbellishments.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embellish.LinqExtensions
+{
+    public static class LinqEmbellishments
+    {
+        public static IEnumerable<T> FirstNMatchingCondition<T>(this IEnumerable<T> original, int numberOfResults, Func<T, bool> predicate)
+        {
+            
+            var count = original.Count(predicate);
+            
+            if (numberOfResults > count)
+            {
+                numberOfResults = count;
+            }
+
+            return original.Where(predicate).Take(numberOfResults);
+        }
+
+        public static IEnumerable<T> FirstN<T>(this IEnumerable<T> original, int numberOfResults)
+        {
+            var count = original.Count();
+
+            if (numberOfResults > count)
+            {
+                numberOfResults = count;
+            }
+
+            return original.Take(numberOfResults);
+        }
+
+        public static IEnumerable<T> LastNMatchingCondition<T>(this IEnumerable<T> original, int numberOfResults, Func<T, bool> predicate)
+        {
+            var count = original.Count(predicate);
+
+            if (numberOfResults > count)
+            {
+                numberOfResults = count;
+            }
+
+            int skip = count - numberOfResults;
+            return original.Where(predicate).Skip(skip).Take(numberOfResults);
+        }
+
+        public static IEnumerable<T> LastN<T>(this IEnumerable<T> original, int numberOfResults)
+        {
+            var count = original.Count();
+
+            if (numberOfResults > count)
+            {
+                numberOfResults = count;
+            }
+
+            int skip = count - numberOfResults;
+            return original.Skip(skip).Take(numberOfResults);
+        }
+
+
+    }
+}
