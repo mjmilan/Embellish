89e7ab7ebed559ed0bee595d8e3055af6001e8c5
martinmi <martin@mjmilan.com>
2014-10-29 10:55:53 +0000

    Added new class for putting extension methods on strings, with functionality for truncating strings, and provided unit tests.

---
+++ Embellish.Tests/StringExtensionTests.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using NUnit.Framework;
+using Embellish;
+namespace Embellish.Tests
+{
+	[TestFixture]
+	public class StringExtensionTests
+	{
+		[Test]
+		public void TruncateNormalUseCase()
+		{
+			string input = "Hello my lovelies!";
+			string output = input.Truncate(5);
+			Assert.That(output, Is.EqualTo("Hello"));
+		}
+		
+		[Test]
+		public void TruncateWithShortString()
+		{
+			string input = "Hello";
+			string output = input.Truncate(34);
+			Assert.That(output, Is.EqualTo(input));
+		}
+		
+		[Test]
+		public void TruncateToSameLength()
+		{
+			string input = "Hello";
+			string output = input.Truncate(5);
+			Assert.That(output, Is.EqualTo(input));
+		}
+	
+		
+	}
+}
---
+++ Embellish/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace Embellish
+{
+	/// <summary>
+	/// This class provides extension methods to instances of the string class.
+	/// </summary>
+	public static class StringExtensions
+	{
+		public static string Truncate(this string input, int size)
+		{
+			if (string.IsNullOrEmpty(input) || input.Length <= size)
+			{
+				return input;
+			}
+			else
+			{
+				return input.Substring(0, size);
+			}
+			
+		}
+	}
+}
