6100f4dba684e6c78e47519dd75e78ae36a38e7f
Martin Milan <martin@mjmilan.com>
2015-07-01 08:01:25 +0100

    Added new XMLElementSubstitution class for substituting XElements.

--- Embellish.Tests/XmlSubstitutionTests.cs
+++ Embellish.Tests/XmlSubstitutionTests.cs
@@ -2,6 +2,7 @@
 using System;
 using NUnit.Framework;
 using System.Xml.Linq;
+using System.Linq;
 
 namespace Embellish.Tests
 {
@@ -13,7 +14,7 @@ namespace Embellish.Tests
 	{
 		#region Members
 		Embellish.XMLSubstitution.XMLSubstitution _xmlSub;
-		
+		Embellish.XMLSubstitution.XMLElementSubstitution _xmlElementSub;
 		#endregion
 		
 		[TestFixtureSetUp]
@@ -21,7 +22,7 @@ namespace Embellish.Tests
 		{
 			//  XName.Get("xml_substitution", "http://www.mjmilan.con/embellish")
 			_xmlSub = new Embellish.XMLSubstitution.XMLSubstitution();
-		
+			_xmlElementSub = new Embellish.XMLSubstitution.XMLElementSubstitution();
 		}
 		
 		[Test]
@@ -40,7 +41,26 @@ namespace Embellish.Tests
 			_xmlSub.MakeSubstitutions();
 			
 			Assert.That(xmlDoc.ToString(), Contains.Substring("Martin Milan"));
+		}
+		
+		[Test]
+		public void ElementSubstitutionTest()
+		{
+			var sb = new System.Text.StringBuilder();
+			sb.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+			sb.AppendLine("<doc xmlns:sub=\"http://www.mjmilan.con/embellish\">");
+			sb.AppendLine(@"<someInfo>Here I am and my name is <sub:xml_substitution>NewElement</sub:xml_substitution></someInfo>");
+			sb.AppendLine("</doc>");
+			var xmlDoc = XDocument.Parse(sb.ToString());
+			var infoSource = new InformationSource();
 			
+			_xmlElementSub.InformationSource = infoSource;
+			_xmlElementSub.DocumentToProcess = xmlDoc;
+			_xmlElementSub.MakeSubstitutions();
+			var xmlString = xmlDoc.ToString();
+			
+			var testResult = xmlDoc.Descendants().Any(x => x.Value == "Martin Milan");
+			Assert.That(testResult, Is.True);
 			
 		}
 	}
@@ -50,6 +70,12 @@ namespace Embellish.Tests
 		public string WhoAmI()
 		{
 			return "Martin Milan";
+		}
+		
+		public XElement NewElement(){
+			var result = new XElement(XName.Get("subed"));
+			result.Value = "Martin Milan";
+			return result;
 		}
 	}
 	
---
+++ Embellish/XMLSubstitution/XMLElementSubstitution.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * User: Martin Milan
+ * Date: 01/07/2015
+ */
+using System;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Linq;
+namespace Embellish.XMLSubstitution
+{
+	/// <summary>
+	/// Description of XMLElementSubstitution.
+	/// </summary>
+	public class XMLElementSubstitution : BaseClasses.XMLSubstitiutionBase
+	{
+		#region Properties
+		public object InformationSource{get; set;}
+		#endregion
+		#region Constructors
+		public XMLElementSubstitution() : base()
+		{
+		}
+		
+		public XMLElementSubstitution(XName nameOfSubstitutionElement, XDocument xmlDoc) : base(nameOfSubstitutionElement, xmlDoc)
+		{
+		
+		}
+		#endregion
+		
+		#region Methods
+		public override void SubstituteMatch(XElement match)
+		{
+			if (this.InformationSource != null)
+			{
+				string content = match.Value;
+				var sourceType = this.InformationSource.GetType();
+				var methodInfo = sourceType.GetMethod(content, BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance);
+				var result = methodInfo.Invoke(this.InformationSource,null) as XElement;
+				if (result != null)
+				{
+					match.AddBeforeSelf(result);
+				}
+				
+				match.Remove();
+			}
+		}
+		#endregion
+	}
+}
